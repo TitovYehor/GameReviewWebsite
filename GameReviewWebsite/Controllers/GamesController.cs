c3f1b63e1da6e28b66660f099fdeb872042baaac
Titov <tegor901@gmail.com>
2024-11-08 17:38:22 +0200

    Add basic website structure

---
+++ GameReviewWebsite/Controllers/GamesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameReviewWebsite.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
--- GameReviewWebsite/Controllers/HomeController.cs
+++ GameReviewWebsite/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace GameReviewWebsite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
